efb0affc68b73edc59a78419ec8233dccaf4e4a3
Jan-Niklas Keck <neowow@online.de>
2013-11-04 17:02:06 +0100

    Update Render.java
    added player2 rendering

--- src/com/solusgames/render/Render.java
+++ src/com/solusgames/render/Render.java
@@ -7,30 +7,59 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
 import com.solusgames.Dogfight_2.Global;
 
+/**
+ * Render class with render loop
+ * Should inherit all things which need rendering
+ * @author keckjs
+ *
+ */
 public class Render {
 
     SpriteBatch batch = new SpriteBatch();
+    SpriteBatch batch1 = new SpriteBatch();
     Texture bg = new Texture(Gdx.files.internal("assets/data/img_bg.jpg"));
 
-    Rectangle glViewport = new Rectangle(0, 0, Global.dim_720.getWidth(),
+    Rectangle viewportPlayer2 = new Rectangle(0, 0, Global.dim_720.getWidth(),
+	    Global.dim_720.getHeight() / 2);
+    Rectangle viewportPlayer1 = new Rectangle(0, Global.dim_720.getHeight() / 2, Global.dim_720.getWidth(),
 	    Global.dim_720.getHeight() / 2);
 
+    /**
+     * Render method
+     */
     public void render() {
-
-	batch.begin();
 	int[] a = new int[1];
 	a[0] = 1;
+	batch.begin();
+	
 	Gdx.gl20.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-	Gdx.gl20.glViewport((int) glViewport.x, (int) glViewport.y,
-		(int) glViewport.width, (int) glViewport.height);
-	batch.setProjectionMatrix(Global.camera_player1.combined);
+	Gdx.gl20.glViewport((int) viewportPlayer1.x, (int) viewportPlayer1.y,
+		(int) viewportPlayer1.width, (int) viewportPlayer1.height);
+	
+	batch.setProjectionMatrix(Global.camera_player1.getCam().combined);
 	// basic bg img
 	batch.draw(bg, 0, 0, Global.map.width * Global.map.tileWidth, Global.map.height * Global.map.tileHeight);
+	
 	Global.player1.render(batch);
-	Global.map_renderer.render(Global.camera_player1, a);
+	Global.player2.render(batch);
+	
+	Global.map_renderer.render(Global.camera_player1.getCam(), a);
+	batch.end();
+	batch1.begin();
+	//Gdx.gl20.glClear(GL20.GL_COLOR_BUFFER_BIT);
+	Gdx.gl20.glViewport((int) viewportPlayer2.x, (int) viewportPlayer2.y,
+		(int) viewportPlayer2.width, (int) viewportPlayer2.height);
+	
+	batch1.setProjectionMatrix(Global.camera_player2.getCam().combined);
+	// basic bg img
+	batch1.draw(bg, 0, 0, Global.map.width * Global.map.tileWidth, Global.map.height * Global.map.tileHeight);
+	Global.player1.render(batch1);
+	Global.player2.render(batch1);
+	
+	Global.map_renderer.render(Global.camera_player2.getCam(), a);
 	// TODO: render loop
 
-	batch.end();
+	batch1.end();
     }
 }
