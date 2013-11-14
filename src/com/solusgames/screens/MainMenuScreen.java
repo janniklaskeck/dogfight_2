7e84421a9a7fe3658d78bf892bab8111c5236d63
Jan-Niklas Keck <neowow@online.de>
2013-11-14 13:09:18 +0100

    Update MainMenuScreen.java
    small changes, added options button

--- src/com/solusgames/screens/MainMenuScreen.java
+++ src/com/solusgames/screens/MainMenuScreen.java
@@ -9,7 +9,6 @@ import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Pixmap.Format;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
@@ -17,12 +16,12 @@ import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.solusgames.controls.Controls;
 
 public class MainMenuScreen implements Screen {
 
     Skin skin;
     Stage stage;
-    SpriteBatch batch;
 
     Game g;
 
@@ -36,7 +35,7 @@ public class MainMenuScreen implements Screen {
     }
 
     public void create() {
-	batch = new SpriteBatch();
+
 	stage = new Stage();
 	Gdx.input.setInputProcessor(stage);
 
@@ -45,9 +44,10 @@ public class MainMenuScreen implements Screen {
 	// recommended solely for the convenience of getting a texture, region,
 	// etc as a drawable, tinted drawable, etc.
 	skin = new Skin();
-	// Generate a 1x1 white texture and store it in the skin named "white".
+	// Generate a 100x100 white texture and store it in the skin named
+	// "white".
 	Pixmap pixmap = new Pixmap(100, 100, Format.RGBA8888);
-	pixmap.setColor(Color.GREEN);
+	pixmap.setColor(Color.WHITE);
 	pixmap.fill();
 
 	skin.add("white", new Texture(pixmap));
@@ -72,26 +72,47 @@ public class MainMenuScreen implements Screen {
 	final TextButton playButton = new TextButton("PLAY", textButtonStyle);
 	playButton.setWidth(300);
 	playButton.setHeight(100);
-	playButton.setPosition(Gdx.graphics.getWidth() / 2 - playButton.getWidth() / 2, 200);
+	playButton.setPosition(
+		Gdx.graphics.getWidth() / 2 - playButton.getWidth() / 2, 300);
 
 	stage.addActor(playButton);
+
+	final TextButton optionsButton = new TextButton("OPTIONS",
+		textButtonStyle);
+	optionsButton.setWidth(300);
+	optionsButton.setHeight(100);
+	optionsButton
+		.setPosition(
+			Gdx.graphics.getWidth() / 2 - optionsButton.getWidth()
+				/ 2, 200);
+
+	stage.addActor(optionsButton);
 
 	final TextButton exitButton = new TextButton("EXIT", textButtonStyle);
 	exitButton.setWidth(300);
 	exitButton.setHeight(100);
-	exitButton.setPosition(Gdx.graphics.getWidth() / 2 - exitButton.getWidth() / 2, 100);
+	exitButton.setPosition(
+		Gdx.graphics.getWidth() / 2 - exitButton.getWidth() / 2, 100);
 
 	stage.addActor(exitButton);
 
 	playButton.addListener(new ChangeListener() {
 	    public void changed(ChangeEvent event, Actor actor) {
 		g.setScreen(new GameScreen());
+		Gdx.input.setInputProcessor(new Controls());
+	    }
+	});
+
+	optionsButton.addListener(new ChangeListener() {
+	    public void changed(ChangeEvent event, Actor actor) {
+		g.setScreen(new OptionsScreen());
 		Gdx.input.setInputProcessor(null);
 	    }
 	});
 
 	exitButton.addListener(new ChangeListener() {
 	    public void changed(ChangeEvent event, Actor actor) {
+		dispose();
 		g.dispose();
 	    }
 	});
@@ -100,7 +121,7 @@ public class MainMenuScreen implements Screen {
     public void render(float delta) {
 	Gdx.gl.glClearColor(0.2f, 0.2f, 0.2f, 1);
 	Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
-	
+
 	stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
 	stage.draw();
 	Table.drawDebug(stage);
@@ -108,7 +129,7 @@ public class MainMenuScreen implements Screen {
 
     @Override
     public void resize(int width, int height) {
-	//stage.setViewport(width, height, false);
+	// stage.setViewport(width, height, false);
     }
 
     @Override
