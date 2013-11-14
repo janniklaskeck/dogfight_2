85b4752c0399ab6e41fd57f6388e29ef7c99f9de
Jan-Niklas Keck <neowow@online.de>
2013-11-14 13:04:04 +0100

    Update Main.java
    removed some unused code

--- src/com/solusgames/Dogfight_2/Main.java
+++ src/com/solusgames/Dogfight_2/Main.java
@@ -1,14 +1,8 @@
 package com.solusgames.Dogfight_2;
 
 import com.badlogic.gdx.ApplicationListener;
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.badlogic.gdx.graphics.GL10;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Image;
-import com.solusgames.screens.menus.MainMenu;
 
 public class Main implements ApplicationListener {
 
@@ -23,33 +17,23 @@ public class Main implements ApplicationListener {
 	cfg.useGL20 = true; // 90% or more of your desktop users should support
 			    // GL20
 
-	new LwjglApplication(new MainMenu(), cfg);
+	new LwjglApplication(new GameMain(), cfg);
     }
 
-    Stage stage;
-    Image image;
 
     @Override
     public void create() {
-	stage = new Stage();
 
-	Texture tex = new Texture("assets/data/libgdx.png");
-	image = new Image(tex);
-
-	stage.addActor(image);
     }
 
     @Override
     public void resize(int width, int height) {
-	stage.setViewport(width, height, true);
+
     }
 
     @Override
     public void render() {
-	Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
 
-	stage.act(Gdx.graphics.getDeltaTime());
-	stage.draw();
     }
 
     @Override
@@ -64,7 +48,7 @@ public class Main implements ApplicationListener {
 
     @Override
     public void dispose() {
-	stage.dispose();
+
     }
 
 }
